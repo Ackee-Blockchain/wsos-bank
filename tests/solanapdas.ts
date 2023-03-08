8d422736502b7f33bb111422a546e668b658df89
Ondrej Rehacek <Ondrejrehacek@gmail.com>
2023-03-08 12:46:49 +0100

    Init

---
+++ tests/solanapdas.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Solanapdas } from "../target/types/solanapdas";
+
+describe("solanapdas", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Solanapdas as Program<Solanapdas>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
